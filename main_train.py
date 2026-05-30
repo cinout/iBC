9493af96e8eb7f4ff4cf68fded0fc0cc4f211b9b
seaskyho <seaskyho@gmail.com>
2026-05-30 20:22:11 +1000

    update

--- main_train.py
+++ main_train.py
@@ -486,7 +486,9 @@ def main(args):
             args.pretrained_ssl_model, map_location=device
         )
         print(f"model:", model)
-        print(f"pretrained_state_dict["state_dict"]:", pretrained_state_dict["state_dict"])
+        print(
+            f"pretrained_state_dict[state_dict]:", pretrained_state_dict["state_dict"]
+        )
         model.load_state_dict(pretrained_state_dict["state_dict"], strict=True)
     model = model.to(device)
 
